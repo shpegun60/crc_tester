7c578fd8bc93fdd682facf40b2859faaa301003d
Ruslan Kovtun <70111075+shpegun60@users.noreply.github.com>
2022-09-30 16:02:11 +0300

    entity framework rewrited starts

--- protocol/entityframework/entity_manager.h
+++ protocol/entityframework/entity_manager.h
@@ -16,17 +16,21 @@ typedef struct Entity Entity;
  */
 
 /**
-  * @brief  entityCallback[x](entity, field, args). Pointer to function array callbacks. [0] -> write callback; [1] -> read callback
+  * @brief  entityCallback(entity, field, args). Pointer to function array callbacks
   * @param  entity - pointer to Entity
   * @param  Field_ptr  - pointer to EntityField
-  * @param  args   - pointer to data for callaback. [0] - write args; [1] - read args (programmer must cast to other last defined value)
+  * @param  args   - pointer to data for callaback (programmer must cast to other last defined value).
   * @retval None
   */
-typedef void (*entityCallback)(Entity* entity, EntityField* field, void* args);
+
+typedef struct {
+    void (*entityCallback)(Entity* entity, EntityField* field, void* context); // callback function
+    void* context;                                                             // callback context
+} entityCallbackContainer;
 
 
 /**************************************************************************************************************************************************
- * bit flags mask for value name "u8 bitFlags" in EntityField
+ * bit flags mask for value name "u16 bitFlags" in EntityField
  */
 #define ENTITY_EMPTY_FLAG    	0x00U // nothing (ordinary field)
 #define ENTITY_READ_ONLY_MSK 	0x01U // its read only field (write protected field)
@@ -35,6 +39,8 @@ typedef void (*entityCallback)(Entity* entity, EntityField* field, void* args);
 #define ENTITY_POINTER_MSK 		0x08U // its pointer, must specify reg in entity_types.h (reading and writing from addres which store in this field)
 #define ENTITY_REGISTER_MSK 	0x10U // its register, must specify reg and ENTITY_PERIF_BUS_TYPE_IND in entity_types.h (bus aligned read and write, it happens for one insrtuction, must uncommitted USE_ENTITY_REGISTER)
 #define ENTITY_ATOMIC_MSK 		0x20U // its atomic field must specify entity_atomic.h file (before write and read disabling interrupts and after restore it, must uncommitted USE_ENTITY_ATOMIC)
+#define ENTITY_LOW_UPDATE_MSK 	0x40U // its atomic field must specify entity_atomic.h file (before write and read disabling interrupts and after restore it, must uncommitted USE_ENTITY_ATOMIC)
+#define ENTITY_READ_ONCE_MSK 	0x80U // its atomic field must specify entity_atomic.h file (before write and read disabling interrupts and after restore it, must uncommitted USE_ENTITY_ATOMIC)
 #define ENTITY_ALL_BIT_MSK 		0xFFU // its mask for counting all fields
 
 /**************************************************************************************************************************************************
@@ -45,28 +51,26 @@ struct EntityField {
 #ifdef USE_ENTITY_CALLBACKS
 
 #ifdef USE_ENTITY_READ_CALLBACK
-    entityCallback rdCallback;      // read callback
-    void *rdArgs;                   // read callback arguments pointer
+    entityCallbackContainer rdCallback; // read callback
 #endif /* USE_ENTITY_READ_CALLBACK */
 
 #ifdef USE_ENTITY_WRITE_CALLBACK
-    entityCallback wrCallback;      // write callback
-    void *wrArgs;                   // write callback arguments pointer
+    entityCallbackContainer wrCallback; // write callback
 #endif /* USE_ENTITY_WRITE_CALLBACK */
 
 #endif /* USE_ENTITY_CALLBACKS */
 
-    u8  bitFlags; //bit[0] - read only, bit[1] - isParam, bit[2] - is log, bit[3] - is pointer ... see bit flags mask
-    u16 shift;
-    u8  type;
-    char descr[ENTITY_DECRIPTION_SIZE];
+    u8      bitFlags; //bit[0] - read only, bit[1] - isParam, bit[2] - is log, bit[3] - is pointer ... see bit flags mask
+    u16     shift;
+    u8      type;
+    char    descr[ENTITY_DECRIPTION_SIZE];
 };
 
 struct Entity {
-    void *pointer;
-    char descr[ENTITY_DECRIPTION_SIZE];
-    u8 fields_count;
-    EntityField* fields;
+    void*           pointer;
+    char            descr[ENTITY_DECRIPTION_SIZE];
+    u8              fields_count;
+    EntityField*    fields;
 };
 
 #endif // ENTITY_MANAGER_H
