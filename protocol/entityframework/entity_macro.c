58f7dd2ede5f36573c58913cf762695216950069
Ruslan Kovtun <70111075+shpegun60@users.noreply.github.com>
2022-10-30 22:03:46 +0200

    added moooooore strange codes

--- protocol/entityframework/entity_macro.c
+++ protocol/entityframework/entity_macro.c
@@ -1,11 +1,11 @@
 #include "entity_macro.h"
 
 #define ENTITY_TUPLE PREPROCESSOR_TUPLE_MAKE(                                           \
-            (entityName_t, "entt", 0, 0, NULL),                                         \
+            (entityName_t, "entt", 0, 0, NULL, (parent1 p1; parent2 p2; parent3 p3;)),                                         \
                                                                                         \
-            (u8,  _name1, (0x00 | 0x01), "des1", 1, 1, 1),                              \
-            (u32,  _name2, (0x00 | 0x01), "des1", 1, 1, 1),                              \
-            (u16, _nameN, (0x00),        "desN", 1, 1, 3)                               \
+            (u8,  _name1, (0x00 | 0x01), "des1", 1, 1, 1, volatile, p1._name1),                              \
+            (u32,  _name2, (0x00 | 0x01), "des1", 2, 1, 1,, p2._name2),                             \
+            (u16, _nameN, (0x00),        "desN", 1, 1, 3,, p3._nameN)                               \
     )
 
 ENTITY_MACRO_EXPAND_STRUCT(ENTITY_TUPLE);
@@ -23,4 +23,4 @@ int entityMactoTest()
     return 0;
 }
 
-#undef ENTITY_TUPLE
+#undef ENTITY_OK
