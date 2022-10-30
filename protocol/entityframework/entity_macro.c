acc51776d5287aa4d7be2146b59bb0b572407e32
Ruslan Kovtun <70111075+shpegun60@users.noreply.github.com>
2022-10-31 00:32:13 +0200

    added parrent supports to entity

--- protocol/entityframework/entity_macro.c
+++ protocol/entityframework/entity_macro.c
@@ -1,14 +1,31 @@
 #include "entity_macro.h"
 
+typedef struct {
+    u8  _name1;
+} parent1;
+
+typedef struct {
+    u32  _name2[2];
+} parent2;
+
+typedef struct {
+    u16  _nameN;
+} parent3;
+
 #define ENTITY_TUPLE PREPROCESSOR_TUPLE_MAKE(                                           \
             (entityName_t, "entt", 0, 0, NULL, (parent1 p1; parent2 p2; parent3 p3;)),                                         \
                                                                                         \
-            (u8,  _name1, (0x00 | 0x01), "des1", 1, 1, 1, volatile, p1._name1),                              \
-            (u32,  _name2, (0x00 | 0x01), "des1", 2, 1, 1,, p2._name2),                             \
-            (u16, _nameN, (0x00),        "desN", 1, 1, 3,, p3._nameN)                               \
+            (u8,  _name1, (0x00 | 0x01), "des1", 1, 1, 1,volatile),                              \
+            (u32, , (0x00 | 0x01), "des1", 2, 1, 1,, p2._name2),                             \
+            (u16, , (0x00),        "desN", 1, 1, 3,, p3._nameN)                               \
     )
 
-ENTITY_MACRO_EXPAND_STRUCT(ENTITY_TUPLE);
+typedef struct {
+    parent1 p1;
+    parent2 p2;
+    parent3 p3;
+    volatile u8 _name1;
+} entityName_t;
 
 int entityMactoTest()
 {
