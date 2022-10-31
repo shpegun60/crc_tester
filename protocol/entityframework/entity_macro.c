b8553366586daea9c47aa6ff235db708611b4b8d
Ruslan Kovtun <70111075+shpegun60@users.noreply.github.com>
2022-10-31 09:40:58 +0200

    nothing

--- protocol/entityframework/entity_macro.c
+++ protocol/entityframework/entity_macro.c
@@ -12,12 +12,12 @@ typedef struct {
     u16  _nameN;
 } parent3;
 
-#define ENTITY_TUPLE PREPROCESSOR_TUPLE_MAKE(                                           \
-            (entityName_t, "entt", 0, 0, NULL, (parent1 p1; parent2 p2; parent3 p3;)),                                         \
-                                                                                        \
-            (u8,  _name1, (0x00 | 0x01), "des1", 1, 1, 1,volatile),                              \
-            (u32, , (0x00 | 0x01), "des1", 2, 1, 1,, p2._name2),                             \
-            (u16, , (0x00),        "desN", 1, 1, 3,, p3._nameN)                               \
+#define ENTITY_TUPLE PREPROCESSOR_TUPLE_MAKE(                                                                                   \
+            (entityName_t, "entt", 0, 0, NULL, (parent1 p1; parent2 p2; parent3 p3;)),                                          \
+                                                                                                                                \
+            (u8,  _name1, (0x00 | 0x01), "des1", 1, 1, 1,volatile),                                                             \
+            (u32, , (0x00 | 0x01), "des1", 2, 1, 1,, p2._name2),                                                                \
+            (u16, , (0x00),        "desN", 1, 1, 3,, p3._nameN)                                                                 \
     )
 
 ENTITY_MACRO_EXPAND_STRUCT(ENTITY_TUPLE);
@@ -32,7 +32,7 @@ int entityMactoTest()
     entityName_t* data_ptr = &data;
 
     ENTITY_MACRO_EXPAND_INIT(return ENTITY_ERROR, entityNumber, fieldNumber, entity, data_ptr, ENTITY_TUPLE);
-    return 0;
+    return ENTITY_OK;
 }
 
-#undef ENTITY_OK
+#undef ENTITY_TUPLE
